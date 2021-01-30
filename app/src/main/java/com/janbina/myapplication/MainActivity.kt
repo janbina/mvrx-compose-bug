e511b6fafcaea2d65250c9b38d7f1453f0c952a6
Jan Bína <binajohny@gmail.com>
2021-01-30 19:14:24 +0100

    Update compose

--- app/src/main/java/com/janbina/myapplication/MainActivity.kt
+++ app/src/main/java/com/janbina/myapplication/MainActivity.kt
@@ -2,13 +2,13 @@ package com.janbina.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
